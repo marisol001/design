6ec5143dcd439c400e90a48f2c452bc926825faa
marisol001 <reyes-espino-marisol@hotmail.com>
2022-09-08 23:43:18 -0500

    Config files

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const TapHomeScreen(),
+      ),
+    );
+  }
+}
+
+class TapHomeScreen extends StatelessWidget {
+  const TapHomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: const [
+        Text('Hello World'),
+        Text('Hello World'),
+      ],
+    );
+  }
+}
